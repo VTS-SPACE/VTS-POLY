a73c25381f373b6013e9aae7574c62dd4327ef9a
VANSH TECHNOLOGY SERVICES <vanshtechnologyservices@gmail.com>
2025-09-09 21:18:30 +0530

    Update download.js

--- download.js
+++ download.js
@@ -3,7 +3,7 @@
 (() => {
     "use strict";
 
-    const APK_URL = 'assets/apk/VTS-POLY-release.apk';
+    const APK_URL = 'https://t.me/c/2919044362/7';
     const SAVE_FILENAME = 'VTS-POLY-v1.0.apk';
     const VERSION = 'v1.0';
     const RELEASE_DATE = '2025-09-02';
